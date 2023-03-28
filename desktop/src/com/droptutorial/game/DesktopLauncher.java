e6032393833459929d1c011cfdeb0799a06e72fc
SergioHerreraMarin <sergioherreramarin@gmail.com>
2023-03-28 15:45:44 +0200

    Setup android project

---
+++ desktop/src/com/droptutorial/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.droptutorial.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.droptutorial.game.DropGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("DropTutorial");
+		new Lwjgl3Application(new DropGame(), config);
+	}
+}
